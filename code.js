3b4f240a6c1bf1ba8c1ffd512e09a94c75eb49f8
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-11-20 12:29:46 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,60 @@
 function flip(array, n) {
-    return array;
+    let start = 0;
+    while (start < n) {
+        [arr[start], arr[n]] = [arr[n], arr[start]];
+        start++;
+        n--;
+    }
 }
 
 // Use only flip() here to manipulate the array
 function pancakeSort(array) {
-    return array;
+    if (array.length < 2){
+        return array;
+    }
+    let curSize = array.length;
+    while (curSize >1){
+        let zero = 0;
+        for (let i =1; i < curSize;i++){
+            if (array[i] > array[zero]){
+                zero=i;
+            }
+        }
+        if (zero!==curSize - 1){
+            flip(array,zero);
+            flip(array,curSize-1)
+        }
+        curSize--;
 }
+for (let i =1; i <array.length;i++){
+    let pos = 0;
+    if (array[i] <=array[0]){
+        if (i ==1){
+            flip(array, i+1);
+        }
+        else{
+            flip(array,i)
+            flip(array,i+1);
+        }
+    }
+    else if (array[i]>array[0] && array[1]<array[i-1]{
+        flip(array,i+1);
+        while (array[0] <= array[pos]){
+            pos++;
+        }
+    flip(array,pos);
+    flip(array,pos -1);
+    flip(array,i+1);
+    }
+}
+    return array;
+
+        
+
+
+
+
+
+
+
+    
